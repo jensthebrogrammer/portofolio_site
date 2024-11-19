26c17b0f1e4d98db4999ef4a6e73b3903f543366
jensthebrogrammer <jenslaurijssen@gmail.com>
2024-11-19 10:40:50 +0100

    did some styling on the nav-bar

--- frontend/src/modules/navbar.jsx
+++ frontend/src/modules/navbar.jsx
@@ -5,16 +5,22 @@ import { HashLink as Link } from "react-router-hash-link";
 export default function Navbar() {
   return (
   <>
-    <nav className="nav-bar p-0">
-      <div className="container nav-bar-element align-items">
-        <Link smooth to="#projecten" className="nav-link text-center pt-1">
+    <nav className="nav-bar p-0 text-center">
+      <div className="container nav-bar-element align-items-center justify-content-center p-3">
+        <Link smooth to="#projecten" className="nav-link text-center">
           Mijn projecten
         </Link>
       </div>
 
-      <div className="container nav-bar-element align-items">
-        <Link smooth to="#info" className="nav-link text-center pt-1">
+      <div className="container nav-bar-element align-items-center justify-content-center p-3">
+        <Link smooth to="#info" className="nav-link text-center">
           Over mij
+        </Link>
+      </div>
+
+      <div className="container nav-bar-element align-items-center justify-content-center">
+        <Link smooth to="#softwareKennis" className="nav-link text-center">
+          Software kennis
         </Link>
       </div>
     </nav>
