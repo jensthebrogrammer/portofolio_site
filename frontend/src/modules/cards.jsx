eebfcdf4da952df075fdab34b7b262525d4742a5
jensthebrogrammer <jenslaurijssen@gmail.com>
2024-11-26 09:49:48 +0100

    extra kaarten toegevoegd

--- frontend/src/modules/cards.jsx
+++ frontend/src/modules/cards.jsx
@@ -53,6 +53,48 @@ export default function MijnProjecten() {
           </div>
 
         </div>
+
+
+        <div className="row justify-content-center my-5">
+          <div className="col-lg-6 col-12 my-3 d-flex justify-content-center p-0">
+            <div className="card bg-dark">
+              <div className="row">
+                <div className="col-5">
+                  <img src='\src\images\printscreen_whack-a-mole.png' className="img-fluid rounded-start img-card" />
+                </div>
+
+                <div className="col-7">
+                  <div className="card-body">
+                    <h3 className="card-title">Whack a mole</h3>
+                    <p className="card-text">Een spel geschreven om te kunnen samenwerken met een spelbord.</p>
+                    <p className="card-text pb-3">Je kan dit spel ook gewoon op de computer spelen met de toetsen ASD. Het is gemaakt in Python met Pygame als framework</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-6 col-12 my-3 d-flex justify-content-center p-0">
+            <div className="card bg-dark">
+              <div className="row">
+                <div className="col-5">
+                  <img src='\src\images\afbeelding_rolstoel.png' className="img-fluid rounded-start img-card" />
+                </div>
+
+                <div className="col-7">
+                  <div className="card-body">
+                    <h3 className="card-title">Rolstoel</h3>
+                    <p className="card-text">Een remote controlled rolstoel.</p>
+                    <p className="card-text pb-3">Ik heb vooral het electronica gedeelte gedaan en het programmeren (arduino).
+                      de rolstoel heeft veschillende bestuuringsmodussen.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </section>
   )
